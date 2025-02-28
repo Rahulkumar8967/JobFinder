38f6a2fd4617c0e240797fd6a33a90d4342083f4
Rahul Kumar Saini <sainirahul8967@gmail.com>
2025-02-28 21:56:21 +0530

    Add frontend setup with SedcnUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
  
   return (
    <>
    <h1>Job-Portal</h1>
+   <Button className="flex flex-col items-center justify-center">Click me</Button>
    </>
   )
 }
